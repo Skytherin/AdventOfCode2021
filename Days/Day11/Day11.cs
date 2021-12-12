f6da28fc46e817517e2e293bebd83963572515cd
Jake Baker <jxbaker@sep.com>
2021-12-12 09:25:57 -0500

    Day 11 and Day12

---
+++ Days/Day11/Day11.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2021.Utils;
+using JetBrains.Annotations;
+
+namespace AdventOfCode2021.Days.Day11
+{
+    [UsedImplicitly]
+    public class Day11 : AdventOfCode<int[][]>
+    {
+        public override string Example => @"5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526";
+
+        public override int[][] Parse(string s) => Convert2d(s.Lines(), col => Convert.ToInt32($"{col}"));
+
+        [TestCase(Input.Example, 1656)]
+        [TestCase(Input.File, 1637)]
+        public override long Part1(int[][] input)
+        {
+            var flashCount = 0;
+            for (var step = 0; step < 100; step++)
+            {
+                input = Convert2d(input, col => col + 1);
+                var flashes = Convert2d(input, _ => false);
+                var anyFlashed = true;
+                while (anyFlashed)
+                {
+                    anyFlashed = false;
+                    foreach (var cell in Cells(input).Where(it => it.Value > 9 && !flashes[it.Row][it.Col]))
+                    {
+                        flashes[cell.Row][cell.Col] = true;
+                        foreach (var adjacent in Adjacents(input, cell.Row, cell.Col))
+                        {
+                            input[adjacent.Row][adjacent.Col] += 1;
+                        }
+
+                        anyFlashed = true;
+                        flashCount += 1;
+                    }
+                }
+
+                input = Convert2d(input, col => col > 9 ? 0 : col);
+            }
+
+            return flashCount;
+        }
+
+        [TestCase(Input.Example, 195)]
+        [TestCase(Input.File, 242)]
+        public override long Part2(int[][] input)
+        {
+            for (var step = 0; step < 1000; step++)
+            {
+                input = Convert2d(input, col => col + 1);
+                var flashes = Convert2d(input, _ => false);
+                var anyFlashed = true;
+                while (anyFlashed)
+                {
+                    anyFlashed = false;
+                    foreach (var cell in Cells(input).Where(it => it.Value > 9 && !flashes[it.Row][it.Col]))
+                    {
+                        flashes[cell.Row][cell.Col] = true;
+                        foreach (var adjacent in Adjacents(input, cell.Row, cell.Col))
+                        {
+                            input[adjacent.Row][adjacent.Col] += 1;
+                        }
+
+                        anyFlashed = true;
+                    }
+                }
+
+                if (Cells(flashes).All(it => it.Value)) return step+1;
+
+                input = Convert2d(input, col => col > 9 ? 0 : col);
+            }
+
+            throw new ApplicationException();
+        }
+
+        private IEnumerable<(int Row, int Col, T Value)> Adjacents<T>(T[][] array, int row, int col)
+        {
+            var rows = array.Length;
+            var columns = array[0].Length;
+
+            foreach (var dY in new[] { -1, 0, 1 })
+            {
+                foreach (var dX in new[] { -1, 0, 1 })
+                {
+                    if (dY != 0 || dX != 0)
+                    {
+                        var nY = row + dY;
+                        var nX = col + dX;
+                        if (nY >= 0 && nX >= 0 && nY < rows && nX < columns) yield return (nY, nX, array[nY][nX]);
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<(int Row, int Col, T Value)> Cells<T>(T[][] array)
+        {
+            return array.WithIndices()
+                .SelectMany(row => row.Value.WithIndices()
+                    .Select(col => (row.Index, col.Index, col.Value)));
+        }
+
+        private T2[][] Convert2d<T, T2>(IEnumerable<IEnumerable<T>> input, Func<T, T2> action)
+        {
+            return input.Select(row => row.Select(action).ToArray()).ToArray();
+        }
+    }
+}
---
+++ Days/Day12/Day12.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2021.Utils;
+using FluentAssertions;
+using JetBrains.Annotations;
+
+namespace AdventOfCode2021.Days.Day12
+{
+    public class Day12 : IAdventOfCode
+    {
+        public void Run()
+        {
+            Part1();
+            Part2();
+        }
+
+        private void Part1()
+        {
+            Do1(@"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end").Should().Be(10);
+
+            Do1(@"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc").Should().Be(19);
+
+            Do1(@"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW").Should().Be(226);
+
+            Do1(@"end-MY
+MY-xc
+ho-NF
+start-ho
+NF-xc
+NF-yf
+end-yf
+xc-TP
+MY-qo
+yf-TP
+dc-NF
+dc-xc
+start-dc
+yf-MY
+MY-ho
+EM-uh
+xc-yf
+ho-dc
+uh-NF
+yf-ho
+end-uh
+start-NF").Should().Be(5076);
+        }
+
+        private int Do1(string s, bool revisitAllowed = false)
+        {
+            var inputs = StructuredRx.ParseLines<ConnectedNodes>(s);
+
+            var caves = inputs.SelectMany(it => new[] { it.Node1, it.Node2 }).Distinct().ToList();
+
+            var adjacencies = caves.ToDictionary(it => it, _ => new HashSet<string>());
+            foreach (var input in inputs)
+            {
+                adjacencies[input.Node1].Add(input.Node2);
+                adjacencies[input.Node2].Add(input.Node1);
+            }
+
+            var smallCaves = caves.Where(it => it.ToLower() == it).ToHashSet();
+            var open = new[] { (Head: "start", Path: new List<string> { "start" } as IReadOnlyList<string>, RevisitAllowed: revisitAllowed) }.ToList();
+            var found = 0;
+            while (open.Any())
+            {
+                var current = open.Shift();
+
+                foreach (var adjacentCave in adjacencies[current.Head])
+                {
+                    var revisit = current.RevisitAllowed;
+                    if (smallCaves.Contains(adjacentCave) && current.Path.Contains(adjacentCave))
+                    {
+                        if (!revisit) continue;
+                        if (adjacentCave == "start") continue;
+                        revisit = false;
+                    }
+
+                    if (adjacentCave == "end")
+                    {
+                        found += 1;
+                    }
+                    else
+                    {
+                        open.Add((Head: adjacentCave, Path: current.Path.Append(adjacentCave).ToList(), RevisitAllowed: revisit));
+                    }
+                }
+            }
+
+            return found;
+        }
+
+
+        private void Part2()
+        {
+            Do1(@"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end", true).Should().Be(36);
+
+            Do1(@"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc", true).Should().Be(103);
+
+            Do1(@"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW", true).Should().Be(3509);
+
+            Do1(@"end-MY
+MY-xc
+ho-NF
+start-ho
+NF-xc
+NF-yf
+end-yf
+xc-TP
+MY-qo
+yf-TP
+dc-NF
+dc-xc
+start-dc
+yf-MY
+MY-ho
+EM-uh
+xc-yf
+ho-dc
+uh-NF
+yf-ho
+end-uh
+start-NF", true).Should().Be(145643);
+        }
+    }
+
+    public class ConnectedNodes
+    {
+        [RxFormat(After = "-")]
+        public string Node1 { get; set; } = "";
+
+        public string Node2 { get; set; } = "";
+    }
+}
